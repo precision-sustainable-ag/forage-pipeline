17b36212cd318140581b691aefd2c3a02cb5d9fe
Brian W. Davis <brianwdavis@gmail.com>
2023-04-24 11:07:52 -0400

    fixup datum calculation; throw out alley during datum calculation

--- labeled_to_datum.R
+++ labeled_to_datum.R
@@ -1,15 +1,63 @@
+library(dplyr)
 
-mode <- function(x) {
+source("secret.R")
+
+
+# Fetch: ----
+blob_ctr <- 
+  AzureStor::blob_container(
+    sas_endpoint, 
+    sas = sas_token
+  ) 
+
+existing_blobs <- 
+  AzureStor::list_blobs(
+    blob_ctr, info = "name"
+  )
+
+blob_csvs <- stringr::str_subset(
+  existing_blobs,
+  glue::glue("{uuid_rx}\\.csv")
+)
+
+AzureStor::multidownload_blob(
+  blob_ctr,
+  blob_csvs,
+  file.path("forage_labeled_blobs", blob_csvs)
+)
+
+hex_rx <- function(...) {
+  n <- c(...)
+  hex <- "[a-fA-F0-9]"
+  glue::glue("{hex}{{{n}}}") %>% 
+    paste0(collapse = "-")
+}
+
+uuid_rx <- hex_rx(8, 4, 4, 4, 12)  
+
+
+most_common <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
 }
 
 
+scans <- dir(
+  "forage_labeled_blobs",
+  full.names = T,
+  pattern = "geojson"
+) %>% 
+  purrr::map(
+    ~sf::st_read(.x),
+  )
+
 # labeled blob to datum ----
 
 track_rm_outliers <- function(trk) {
 
-  trk_clean <- trk |> filter(!is.na(SONAR), !is.na(LIDAR)) |> 
+  trk_clean <- 
+    trk %>%  
+    filter(!is.na(SONAR), !is.na(LIDAR)) %>%  
     filter(SONAR < 999, LIDAR < 999)
   
   lidar_dist = MASS::fitdistr(trk_clean$LIDAR, 'normal')
@@ -33,27 +81,46 @@ track_rm_outliers <- function(trk) {
 }
 
 track_add_datum <- function(trk) {
+  
   trk_lidar_top_decile <-
     trk %>%
+    filter(!(flag == 0 & str_detect(fn, "_ce1_|_ce2_"))) %>% 
     filter(cume_dist(LIDAR) > 0.89) %>%
     pull(LIDAR) |> 
-    mode()
+    most_common()
   
   trk_sonar_top_decile <-
     trk %>%
+    filter(!(flag == 0 & str_detect(fn, "_ce1_|_ce2_"))) %>% 
     filter(cume_dist(SONAR) > 0.89) %>%
     pull(SONAR) |> 
-    mode()
-  
-  
+    most_common()
+
+  # TODO why are we using different numbers for the datum for each sensor?
+  #   They're the same height above the ground, so if LIDAR finds the 
+  #   ground more often, use that number to subtract from both sensors?
+
   trk %>%
-    mutate(lidar_datum = trk_lidar_top_decile) %>%
-    mutate(sonar_datum = trk_sonar_top_decile) %>%
-    mutate(lidar_ht = lidar_datum - LIDAR) %>%
-    mutate(sonar_ht = sonar_datum - SONAR)
+    mutate(lidar_dat = trk_lidar_top_decile) %>%
+    mutate(sonar_dat = trk_sonar_top_decile) %>%
+    mutate(lidar_ht = lidar_dat - LIDAR) %>%
+    mutate(sonar_ht = sonar_dat - SONAR) %>% 
+    mutate(l2s_ratio = LIDAR/SONAR)
   
   # TODO: Don't we need coefficients for these units to get cm?
+  # Only have coefficients for the 211. For the 214 they're precomputed.
+  # For 211, the lasers are divided by 3.1 to get cm and the sonic is x/15.6
 }
+
+ce2_with_height <- 
+  ce2_plots %>% 
+  purrr::map("result") %>% 
+  purrr::compact() %>% .[1] %>% 
+# scans %>% 
+  purrr::map(
+    ~track_rm_outliers(.x) %>%
+      track_add_datum()
+  )
 
 
 onfarm_loops_with_heights <- 
