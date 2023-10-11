b5ca1942f30f580d02a27c04de761dfb1c91265f
Brian W. Davis <brianwdavis@gmail.com>
2023-10-11 14:54:35 -0400

    implement brown datum in summaries; handle uploads/downloads; handle all-NA cols

--- datum_to_summary_bm.R
+++ datum_to_summary_bm.R
@@ -1,34 +1,97 @@
 library(dplyr)
 library(moments)
 
+source("secret.R")
+
+replace_ext <- function(fn, ext) {
+  paste0(tools::file_path_sans_ext(fn), ".", ext)
+}
+
+blob_ctr <- 
+  AzureStor::list_blob_containers(
+    sas_endpoint, 
+    sas = sas_token
+  )[["02-plots-with-datum"]] 
+
+existing_datum_blobs <- 
+  AzureStor::list_blobs(
+    blob_ctr, info = "name"
+  )
+
+
+
+
+summarized_blobs <- 
+  AzureStor::list_blobs(
+    AzureStor::list_blob_containers(
+      sas_endpoint, 
+      sas = sas_token
+    )[["03-plots-with-summary"]], 
+    info = "name"
+  ) %>% 
+  stringr::str_extract(uuid_rx) %>% 
+  unique()
+
+
+datum_geojsons <- 
+  stringr::str_subset(
+    existing_datum_blobs,
+    glue::glue("{uuid_rx}\\.geojson")
+  ) %>% 
+  filter_uuids(summarized_blobs)
+
+
+
+AzureStor::multidownload_blob(
+  blob_ctr,
+  datum_geojsons,
+  file.path("blobs_without_summary", datum_geojsons),
+  overwrite = T
+)
+
+
 # datum to summaries ----
 
 col_prop <- function(x, brks, nm) {
   lbl <- paste(nm, head(brks, -1), tail(brks, -1), sep = "_")
-  x_lbl <- cut(x, brks, lbl, right = F)
   
-  (table(x_lbl)/length(x)) |> 
-    bind_rows() |> 
+  if (all(is.na(x))) {
+    ret <- rep(NA, length(lbl)) %>% 
+      purrr::set_names(lbl) 
+  } else {
+      x_lbl <- cut(x, brks, lbl, right = F)
+      
+      ret <- (table(x_lbl)/length(x)) 
+  }
+  
+  ret %>% 
+    bind_rows() %>% 
     mutate_all(as.numeric)
 }
 
 
 summarise_plot <- function(trk) {
-  trk <- trk |> filter(flag != 0) |> group_by(flag)
-  
+  trk <- trk |> group_by(flag)
+
   flags_with_props <- 
     trk |> 
     summarise(
       ndvi = col_prop(NDVI, c(0.1, 0.29, 0.4, 0.53, 0.69, 1), "ndvi"),
       sonar = col_prop(sonar_ht, c(0, 5, 10, 15, 20, 40, Inf), "sonar"),
-      lidar = col_prop(lidar_ht, c(0, 5, 10, 15, 20, 30, 40, Inf), "lidar")
+      lidar = col_prop(lidar_ht, c(0, 5, 10, 15, 20, 30, 40, Inf), "lidar"),
+      sonar_brown = col_prop(sonar_ht_brown, c(0, 5, 10, 15, 20, 40, Inf), "sonar_brown"),
+      lidar_brown = col_prop(lidar_ht_brown, c(0, 5, 10, 15, 20, 30, 40, Inf), "lidar_brown")
     ) |> 
-    tidyr::unnest(cols = c(ndvi, sonar, lidar))
+    tidyr::unnest(cols = c(ndvi, sonar, lidar, sonar_brown, lidar_brown))
   
   flags_with_moments <- 
     trk |> 
+    mutate_at(
+      vars(NDVI, sonar_ht, lidar_ht, sonar_ht_brown, lidar_ht_brown),
+      as.numeric
+    ) %>% 
     summarise_at(
-      vars(NDVI, sonar_ht, lidar_ht),
+      vars(NDVI, sonar_ht, lidar_ht, sonar_ht_brown, lidar_ht_brown),
       list(
         mean = ~mean(.),
         cv = ~mean(.)/sd(.),
@@ -46,16 +109,72 @@ summarise_plot <- function(trk) {
     select(fn, everything())
 }
 
-onfarm_summaries <- 
-  onfarm_loops_with_heights |>
+datum_sf <- dir(
+  "blobs_without_summary",
+  pattern = "geojson$",
+  full.names = T
+) %>% 
+  purrr::map(read_sf)
+
+
+
+scan_summaries <- 
+  datum_sf |>
   purrr::map(
     ~st_drop_geometry(.x) |> 
-      summarise_plot()
+      purrr::safely(summarise_plot)()
   )
 
+purrr::map(scan_summaries, "error") %>% 
+  purrr::compact()
+
+# upload ----
+scan_summaries %>% 
+  purrr::map("result") %>% 
+  purrr::compact() %>% 
+  purrr::map(
+    ~{
+      fn <- basename(.x$fn[1]) %>% 
+        replace_ext("geojson")
+      
+      if (is.na(fn)) { browser() }
+      
+      sf::st_write(
+        .x, file.path("plots_with_summary", fn),
+        delete_dsn = T
+      )
+    }
+  )
+
+sum_ctr <- AzureStor::list_blob_containers(
+  sas_endpoint, 
+  sas = sas_token
+)[["03-plots-with-summary"]] 
+
+# TODO: do/don't overwrite existing blobs?
+
+summarized_plots_pushed <- 
+  dir(
+    "plots_with_summary",
+    full.names = T,
+    pattern = "geojson"
+  ) %>%
+  purrr::map(
+    ~purrr::safely(forage_upload)(.x, sum_ctr),
+  )
+
+summarized_plots_pushed %>% 
+  purrr::map("error") %>% 
+  purrr::compact()
 
 
 
+
+
+
+
+#TODO divorce this into datum to summary vs summary plus biomass
+# Milad will do the biomass part ----
 
 library(httr)
 # datum plots to biomass ----
