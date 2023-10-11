dbdd387f43be7d617e0c366cc92fe802cea4e02d
Brian W. Davis <brianwdavis@gmail.com>
2023-10-11 14:55:23 -0400

    remove points more than 2.5m away from sensor; add "brown" datum cols;

--- labeled_to_datum.R
+++ labeled_to_datum.R
@@ -21,6 +21,9 @@ filter_uuids <- function(x, targets) {
   x[!(x_uuid %in% targets)]
 }
 
+replace_ext <- function(fn, ext) {
+  paste0(tools::file_path_sans_ext(fn), ".", ext)
+}
 
 unlink("plots_with_datum", recursive = T)
 dir.create("plots_with_datum")
@@ -86,15 +89,18 @@ scans <- dir(
   )
 
 # labeled blob to datum ----
-# TODO remove SONAR and LIDAR where it's more than 2.5m
-#   before removing other outliers
+
+
 track_rm_outliers <- function(trk) {
 
   trk_clean <- 
     trk %>%  
     filter(!(flag == 0 & str_detect(fn, "_ce1_|_ce2_"))) %>% 
+    # TODO we need to filter the tip-up slope from CE1/2 before removing
+    # outliers, but the logic isn't yet clear
     filter(!is.na(SONAR), !is.na(LIDAR)) %>%  
-    filter(SONAR < 999, LIDAR < 999)
+    filter(SONAR < 999, LIDAR < 999) %>% 
+    filter(SONAR <= 250, LIDAR <= 250)
   
   lidar_dist = MASS::fitdistr(trk_clean$LIDAR, 'normal')
   lidar_mean = as.numeric(lidar_dist$estimate[1])
@@ -122,6 +128,8 @@ track_rm_outliers <- function(trk) {
   trk_outliers_removed
 }
 
+
+
 track_add_datum <- function(trk) {
   
   trk_lidar_top_decile <-
@@ -137,6 +145,22 @@ track_add_datum <- function(trk) {
     filter(cume_dist(SONAR) > 0.89) %>%
     pull(SONAR) |> 
     most_common()
+  
+  trk_lidar_top_decile_brown <-
+    trk %>%
+    filter(!(flag == 0 & str_detect(fn, "_ce1_|_ce2_"))) %>% 
+    filter(cume_dist(LIDAR) > 0.89) %>%
+    filter(NDVI <= 0.3) %>% 
+    pull(LIDAR) |> 
+    most_common()
+  
+  trk_sonar_top_decile_brown <-
+    trk %>%
+    filter(!(flag == 0 & str_detect(fn, "_ce1_|_ce2_"))) %>% 
+    filter(cume_dist(SONAR) > 0.89) %>%
+    filter(NDVI <= 0.3) %>% 
+    pull(SONAR) |> 
+    most_common()
 
   # TODO why are we using different numbers for the datum for each sensor?
   #   They're the same height above the ground, so if LIDAR finds the 
@@ -147,11 +171,11 @@ track_add_datum <- function(trk) {
     mutate(sonar_dat = trk_sonar_top_decile) %>%
     mutate(lidar_ht = lidar_dat - LIDAR) %>%
     mutate(sonar_ht = sonar_dat - SONAR) %>% 
+    mutate(lidar_dat_brown = trk_lidar_top_decile_brown) %>%
+    mutate(sonar_dat_brown = trk_sonar_top_decile_brown) %>%
+    mutate(lidar_ht_brown = lidar_dat_brown - LIDAR) %>%
+    mutate(sonar_ht_brown = sonar_dat_brown - SONAR) %>% 
     mutate(l2s_ratio = LIDAR/SONAR)
-  
-  # TODO: Don't we need coefficients for these units to get cm?
-  # Only have coefficients for the 211. For the 214 they're precomputed.
-  # For 211, the lasers are divided by 3.1 to get cm and the sonic is x/15.6
 }
 
 
@@ -162,9 +186,20 @@ labeled_plots_without_outliers <-
     )
 
 labeled_plots_without_outliers %>% 
-  purrr::map("error")
+  purrr::map("error") %>% 
+  purrr::compact()
+
+labeled_plots_without_outliers %>% 
+  purrr::map("error") %>% 
+  purrr::imap(~{if (is.null(.x)) return(NULL) else return(.y)}) %>% 
+  purrr::compact()
+
+scans[[129]] %>%
+  ggplot(aes(color = SONAR)) + 
+  geom_sf()
 
 
+# TODO check error, is it grouping correctly? ----
 labeled_plots_with_datum_to_push <- 
   labeled_plots_without_outliers %>% 
   purrr::map("result") %>% 
@@ -176,7 +211,7 @@ labeled_plots_with_datum_to_push %>%
   purrr::map(
     ~{
       fn <- basename(.x$fn[1]) %>% 
-        str_replace(".csv$", ".geojson")
+        replace_ext("geojson")
       if (is.na(fn)) { browser() }
       
       sf::st_write(
@@ -191,7 +226,7 @@ lbl_ctr <- AzureStor::list_blob_containers(
   sas = sas_token
 )[["02-plots-with-datum"]] 
 
-# TODO: don't overwrite existing blobs?
+# TODO: do/don't overwrite existing blobs?
 
 labeled_plots_with_datum_pushed <- 
   dir(
