00dbbb0344534b1c07ddf3245198453ac11b0ed0
Brian W. Davis <brianwdavis@gmail.com>
2024-01-31 13:53:17 -0500

    cleanup; strip trial summaries

--- datum_to_summary_bm.R
+++ datum_to_summary_bm.R
@@ -7,6 +7,11 @@ replace_ext <- function(fn, ext) {
   paste0(tools::file_path_sans_ext(fn), ".", ext)
 }
 
+unlink("blobs_without_summary", recursive = T)
+dir.create("blobs_without_summary")
+unlink("plots_with_summary", recursive = T)
+dir.create("plots_with_summary")
+
 blob_ctr <- 
   AzureStor::list_blob_containers(
     sas_endpoint, 
@@ -18,6 +23,10 @@ existing_datum_blobs <-
     blob_ctr, info = "name"
   )
 
+# TODO:
+# there are duplicated packages for WCC, 
+#   looks like 20211220, 20211109 maybe?
+# need to hash all the scan files and make sure they're dupes and pick one
 
 
 
@@ -71,21 +80,28 @@ col_prop <- function(x, brks, nm) {
 
 
 summarise_plot <- function(trk) {
-  trk <- trk |> group_by(flag)
+  trk <- trk %>% 
+    group_by(
+      flag, 
+      across(matches("Species")), 
+      across(matches("Rep")),
+      across(matches("plotID"))
+      )
 
   flags_with_props <- 
-    trk |> 
+    trk %>% 
     summarise(
       ndvi = col_prop(NDVI, c(0.1, 0.29, 0.4, 0.53, 0.69, 1), "ndvi"),
       sonar = col_prop(sonar_ht, c(0, 5, 10, 15, 20, 40, Inf), "sonar"),
       lidar = col_prop(lidar_ht, c(0, 5, 10, 15, 20, 30, 40, Inf), "lidar"),
       sonar_brown = col_prop(sonar_ht_brown, c(0, 5, 10, 15, 20, 40, Inf), "sonar_brown"),
-      lidar_brown = col_prop(lidar_ht_brown, c(0, 5, 10, 15, 20, 30, 40, Inf), "lidar_brown")
-    ) |> 
+      lidar_brown = col_prop(lidar_ht_brown, c(0, 5, 10, 15, 20, 30, 40, Inf), "lidar_brown"),
+      .groups = "keep"
+    ) %>%  
     tidyr::unnest(cols = c(ndvi, sonar, lidar, sonar_brown, lidar_brown))
   
   flags_with_moments <- 
-    trk |> 
+    trk %>% 
     mutate_at(
       vars(NDVI, sonar_ht, lidar_ht, sonar_ht_brown, lidar_ht_brown),
       as.numeric
@@ -102,9 +118,8 @@ summarise_plot <- function(trk) {
 
   full_join(
     flags_with_moments, 
-    flags_with_props,
-    by = "flag"
-  ) |> 
+    flags_with_props
+  ) %>% 
     mutate(fn = trk$fn[1]) |> 
     select(fn, everything())
 }
@@ -119,16 +134,18 @@ datum_sf <- dir(
 
 
 scan_summaries <- 
-  datum_sf |>
+  datum_sf %>% 
   purrr::map(
-    ~st_drop_geometry(.x) |> 
+    ~st_drop_geometry(.x) %>% 
       purrr::safely(summarise_plot)()
   )
 
 purrr::map(scan_summaries, "error") %>% 
   purrr::compact()
 
-#TODO delete existing blobs and write csvs instead
+purrr::map(scan_summaries, "result") %>% 
+  purrr::compact()
+
 # upload ----
 scan_summaries %>% 
   purrr::map("result") %>% 
@@ -168,51 +185,3 @@ summarized_plots_pushed %>%
   purrr::compact()
 
 
-
-
-
-
-
-#TODO divorce this into datum to summary vs summary plus biomass
-# Milad will do the biomass part ----
-
-library(httr)
-# datum plots to biomass ----
-onfarm_biomass_rq <- GET(
-  "https://api.precisionsustainableag.org/onfarm/biomass",
-  query = list(
-    output = "json",
-    subplot = "separate"
-  ),
-  add_headers("x-api-key" = readLines("api_token_example.txt"))
-)
-
-onfarm_biomass <- 
-  content(onfarm_biomass_rq, as = "text") |> 
-  jsonlite::fromJSON()
-
-
-onfarm_with_bm <- 
-  bind_rows(onfarm_summaries) |> 
-  mutate(fn = basename(fn)) |> 
-  tidyr::separate(
-    fn,
-    c("box", "proj", "location", "timing", NA, 
-      "rep", NA, "scan_date", "uuid"),
-    sep = "_"
-  ) |> 
-  filter(timing == "biomass") |> 
-  mutate(
-    rep = str_remove_all(rep, "rep")
-  ) |> 
-  left_join(
-    onfarm_biomass,
-    by = c("location" = "code", "rep" = "subplot")
-  )
-
-
-onfarm_with_bm |> 
-  ggplot(aes(sonar_ht_mean, uncorrected_cc_dry_biomass_kg_ha)) +
-  geom_point()
-
-readr::write_csv(onfarm_with_bm, "onfarm_2022_scans_with_biomass.csv")
